d2195886b0deed91b72fcc384c3066e89e6ec365
jff <joaofnfernandes@gmail.com>
2014-07-06 21:02:49 +0100

    bug fixing and cleanup

--- 2/main.c
+++ 2/main.c
@@ -12,59 +12,67 @@
 #define false 0
 
 /*---------- Declarations  ----------*/
-void run();
+world_t* process_input();
+void output_response(world_t* world);
 
 
 
 int main(int argc, char** argv) {
 	//run_tests();
-	run();
+	output_response(process_input());
 	return 0;
 }
 
 
 /*------------ Implementation -----------*/
-void run() {
+world_t* process_input() {
 	int size = 0;
 	if(scanf("%d", &size) < 0) {
 		fprintf(stderr, "%s\n", "Define how many cubes there are, at the beginning of the file");
 	}
 	world_t* world = world_create(size);
 	char str1[10]; char str2[10];
-	int cube1 = 0, cube2 = 0;
+	int block1 = 0, block2 = 0;
 	
 	while(true) {
 		scanf("%s", str1);
 		if (strcmp(str1, "quit") == 0) {
 			break;
 		}
-		scanf("%d %s %d", &cube1, str2, &cube2);
+		scanf("%d %s %d", &block1, str2, &block2);
 		/* Pile */
 		if (strcmp(str1, "pile") == 0) {
 			/* Onto */
-			if (strcmp(str2, "onto")) {
-				pile_onto(world, cube1, cube2);
+			if (strcmp(str2, "onto") == 0) {
+				pile_onto(world, block1, block2);
 			} 
 			/* Over */
 			else {
-				pile_over(world, cube1, cube2);
+				pile_over(world, block1, block2);
 			}
 		}
 		/* Move */
 		else {
 			/* Onto */
-			if (strcmp(str2, "onto")) {
-				move_onto(world, cube1, cube2);
+			if (strcmp(str2, "onto") == 0) {
+				move_onto(world, block1, block2);
 			}
 			/* Over */
 			else {
-				move_over(world, cube1, cube2);
+				move_over(world, block1, block2);
 			}
 		}
 	}
+	return world;
 }
 
-
+void output_response(world_t* world){
+	for (int i = 0; i < world->size; i++) {
+		printf("%d:", i);
+		world_print_stack(world, i);
+		printf("\n");
+	}
+}
 
 #endif
 
